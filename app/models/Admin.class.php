d42ddbe2ed07c33367655ebdd05d43cc3f7ff550
dalessr <dale.sunshengran@gmail.com>
2018-04-07 10:03:19 +0800

    Add model for admin page

---
+++ app/models/Admin.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class Admin {
+    /**
+     * @return array 2D-array representing all pets belonging to the current user.
+     */
+    public static function myPets(): array {
+        if (!isset($_SESSION['username'])) {
+            return array();
+        }
+        $db = new Database();
+        $query = "SELECT * FROM pets WHERE username = ?";
+        $result = $db->query($query, array($_SESSION['username']));
+        return $result;
+    }
+
+    public static function add(): bool {
+        return true;
+    }
+
+    public static function queryType(): array {
+        $db = new Database();
+        $query = "SELECT type FROM pet_types";
+
+        return $db->query($query, array());
+    }
+}
