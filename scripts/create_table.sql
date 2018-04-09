9484cf6fcaec45db3c52a25342d817fe8cf0601b
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-09 19:52:10 +0800

    Fix some schema design mistakes

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -29,9 +29,7 @@ CREATE TABLE users (
   bio varchar(511),
   is_admin boolean NOT NULL DEFAULT false,
   is_active boolean NOT NULL DEFAULT true,
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp
 );
 
@@ -39,9 +37,7 @@ CREATE TABLE user_profiles (
   username varchar(255) REFERENCES users(username),
   type user_type NOT NULL,
   score numeric DEFAULT 0,
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp,
   PRIMARY KEY (username, type)
 );
@@ -50,9 +46,7 @@ CREATE TABLE pet_types (
   type varchar(255) PRIMARY KEY,
   root_type varchar(255) REFERENCES pet_types(type),
   description varchar(255),
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp
 );
 
@@ -64,9 +58,7 @@ CREATE TABLE pets (
   avatar varchar(255),
   birthday date,
   bio varchar(511),
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp,
   PRIMARY KEY (username, pet_name)
 );
@@ -78,57 +70,49 @@ CREATE TABLE service_offers (
   end_date date,
   decision_deadline timestamp,
   expected_salary int NOT NULL DEFAULT 100,
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp,
   CONSTRAINT "Decision deadline must be in the future." CHECK (decision_deadline > updated_at),
   CONSTRAINT "Service start time must be after decision deadline." CHECK (start_date > decision_deadline),
   CONSTRAINT "Service end time must be after start time." CHECK (end_date > start_date)
 );
 
+CREATE TABLE service_target (
+  service_id int REFERENCES service_offers(service_id),
+  type varchar(255) REFERENCES pet_types(type),
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp DEFAULT current_timestamp,
+  PRIMARY KEY (service_id, type)
+);
+
+CREATE TABLE bidding (
+  service_id int REFERENCES service_offers(service_id),
+  bidder varchar(255) REFERENCES users(username),
+  points int DEFAULT 0,
+  status bidding_status DEFAULT 'pending',
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp DEFAULT current_timestamp,
+  PRIMARY KEY (bidder, service_id)
+);
+
 CREATE TABLE service_history (
-  service_id int PRIMARY KEY REFERENCES service_offers(service_id),
-  owner varchar(255),
-  taker varchar(255),
+  service_id int PRIMARY KEY,
+  owner varchar(255) NOT NULL,
   rating_for_owner integer,
   review_for_owner varchar(511),
   rating_for_taker integer,
   review_for_taker varchar(511),
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
-  updated_at timestamp DEFAULT current_timestamp
-);
-
-CREATE TABLE service_target (
-  service_id int REFERENCES service_offers(service_id),
-  type varchar(255) REFERENCES pet_types(type),
-  created_by varchar(255) REFERENCES users(username),
-  created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp,
-  PRIMARY KEY (service_id, type)
+  FOREIGN KEY (service_id, owner) REFERENCES bidding(service_id, bidder),
+  CONSTRAINT "Rating for pet owner is not valid." CHECK (rating_for_owner > 0 AND rating_for_owner <= 5),
+  CONSTRAINT "Rating for care taker is not valid." CHECK (rating_for_taker > 0 AND rating_for_taker <= 5)
 );
 
 CREATE TABLE notification (
   username varchar(255) REFERENCES users(username),
   message varchar(511),
   status notification_status DEFAULT 'unread',
-  created_by varchar(255) REFERENCES users(username),
   created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
   updated_at timestamp DEFAULT current_timestamp
-);
-
-CREATE TABLE bidding (
-  bidder varchar(255) REFERENCES users(username),
-  service_id int REFERENCES service_offers(service_id),
-  points int DEFAULT 0,
-  status bidding_status DEFAULT 'pending',
-  created_by varchar(255) REFERENCES users(username),
-  created_at timestamp DEFAULT current_timestamp,
-  updated_by varchar(255) REFERENCES users(username),
-  updated_at timestamp DEFAULT current_timestamp,
-  PRIMARY KEY (bidder, service_id)
 );
--- scripts/create_trigger.sql
+++ scripts/create_trigger.sql
@@ -36,8 +36,30 @@ END; $$
 LANGUAGE PLPGSQL;
 
 -- Validation when inserting a new row into "service_offers" table.
-CREATE TRIGGER pet_owner_valid_type
+CREATE TRIGGER service_provider_valid_type
 BEFORE INSERT OR UPDATE
 ON service_offers
 FOR EACH ROW
-EXECUTE PROCEDURE check_service_provider_owner_type();
+EXECUTE PROCEDURE check_service_provider_type();
+
+-- Checks that the bidder has a user profile of "Pet Owner".
+CREATE OR REPLACE FUNCTION check_bidding_type()
+RETURNS TRIGGER AS $$
+DECLARE
+	count integer;
+BEGIN
+	SELECT COUNT(*) INTO count FROM user_profiles p WHERE p.username = NEW.username AND p.type = 'owner';
+	IF count <= 0 THEN
+		RAISE EXCEPTION '% is not a care taker, thus he/she cannot participate in bidding.', NEW.username;
+		RETURN NULL;
+	END IF;
+	RETURN NEW;
+END; $$
+LANGUAGE PLPGSQL;
+
+-- Validation when inserting a new row into "bidding" table.
+CREATE TRIGGER bidding_valid_type
+BEFORE INSERT OR UPDATE
+ON bidding
+FOR EACH ROW
+EXECUTE PROCEDURE check_bidding_type();
