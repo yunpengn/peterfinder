48bbd204c55b0187ae24fbd28b92a991875bde16
dalessr <dale.sunshengran@gmail.com>
2018-04-09 23:53:37 +0800

    Edit model for listing of bidding data

--- app/models/Bidding.class.php
+++ app/models/Bidding.class.php
@@ -10,7 +10,20 @@ class Bidding {
             return array();
         }
         $db = new Database();
-        $query = "SELECT * FROM bidding WHERE bidder = ?";
+        $query = "SELECT B.bidder as bidder, B.points as points, B.status as status, S.provider as provider, S.start_date as start_date, S.end_date as end_date, S.decision_deadline as decision_deadline, S.expected_salary as expected_salary FROM bidding B inner join service_offers S on B.service_id = S.service_id WHERE bidder = ?";
+        $result = $db->query($query, array($_SESSION['username']));
+        return $result;
+    }
+
+    /**
+     * @return array 2D-array representing all biddings for services offered by the current user.
+     */
+    public static function othersBiddings(): array {
+        if (!isset($_SESSION['username'])) {
+            return array();
+        }
+        $db = new Database();
+        $query = "SELECT B.bidder as bidder, B.points as points, B.status as status, S.provider as provider, S.start_date as start_date, S.end_date as end_date, S.decision_deadline as decision_deadline, S.expected_salary as expected_salary FROM bidding B inner join service_offers S on B.service_id = S.service_id WHERE provider = ?";
         $result = $db->query($query, array($_SESSION['username']));
         return $result;
     }
