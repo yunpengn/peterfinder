9a6d987cd218901087f7d8c34228cdde8e24597b
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-09 10:56:48 +0800

    Add missing files

---
+++ app/models/PetType.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neiln
+ * Date: 2018/04/09
+ * Time: 10:06
+ */
+class PetType {
+    public static function getAllTypes(): array {
+        $db = new Database();
+        $query = "SELECT type, root_type FROM pet_types";
+        $result = $db->query($query, array());
+        return $result;
+    }
+}
