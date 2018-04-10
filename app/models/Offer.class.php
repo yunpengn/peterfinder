ed599e4e6e97413a297bd217be2dcaae8e055df6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-04-10 09:35:25 +0800

    Embed into transaction

--- app/models/Offer.class.php
+++ app/models/Offer.class.php
@@ -67,16 +67,13 @@ class Offer {
 		$db = new Database();
 
         // First updates the row in the service_offers table.
-		$query = "UPDATE service_offers SET start_date = ?, end_date = ?, decision_deadline = ?, expected_salary = ?".
-			"WHERE service_id = ?";
-		$params = array($start_date, $end_date, $decision_deadline, $expected_salary, $service_id);
-		if (!$db->insertOrUpdate($query, $params)) {
-		    return false;
-        }
+		$queries = array("UPDATE service_offers SET start_date = ?, end_date = ?, decision_deadline = ?, ".
+			"expected_salary = ? WHERE service_id = ?");
+		$params = array(array($start_date, $end_date, $decision_deadline, $expected_salary, $service_id));
 
         // Then updates the related rows in the service_target table.
-        $queries = array("DELETE FROM service_target WHERE service_id = ?");
-        $params = array(array($service_id));
+        array_push($queries, "DELETE FROM service_target WHERE service_id = ?");
+        array_push($params, array($service_id));
         foreach ($type_selected as $type) {
             array_push($queries, "INSERT INTO service_target (service_id, type) VALUES (?, ?)");
             array_push($params, array($service_id, $type));
