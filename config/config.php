610e4903574826ef3ab02c8b34b3b7fe741de917
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-06-23 16:00:14 +0800

    Add config file for production

---
+++ config/config.php
@@ -0,0 +1,38 @@
+<?php
+// If you are using Microsoft SQL Server, change DB_TYPE into 'sqlsrv'.
+// If you are using MySQL, change DB_TYPE into 'mysql'.
+define('DB_TYPE', 'pgsql', true);
+define('DB_PREFIX', '', true);
+
+// Retrieve connection string from Heroku variables.
+$db = parse_url(getenv("DATABASE_URL"));
+
+// Settings that are required for connecting to the database server.
+define('DB_HOST', $db["host"], true);
+define('DB_PORT', $db["port"], true);
+define('DB_NAME', ltrim($db["path"], "/"), true);
+define('DB_USER', $db["user"], true);
+define('DB_PASSWORD', $db["pass"], true);
+
+// Gets the DSN to help create connection to the database.
+// Notice that the DSN for Microsoft SQL Server is slightly different.
+if (DB_TYPE == 'sqlsrv') {
+    $dsn = DB_TYPE . ':server=' . DB_HOST . ';database=' . DB_NAME;
+} else if (DB_TYPE == 'pgsql') {
+    $dsn = DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME;
+} else {
+    $dsn = DB_TYPE . ':server = tcp:' . DB_HOST . '; Database=' . DB_NAME;
+}
+define('DSN', $dsn, true);
+
+// Defines the root path for this app.
+// Change to http://localhost:8080/peterfinder on Mac.
+define('APP_URL', 'https://peterfinder.herokuapp.com');
+
+// Settings for sending non-reply emails.
+define('EMAIL_ADDRESS', 'random@gmail.com', true);
+define('EMAIL_PASSWORD', '123456', true);
+
+// Settings for datetime format.
+define('DATE_FORMAT', 'd M, Y', true);
+define('TIME_FORMAT', 'H:m d M, Y', true);
